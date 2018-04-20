d1521492ee9de171cd733c097b6891ba88f0fb1c
root <cheiksiramakankeita@gmail.com>
2018-04-20 11:15:18 +0200

    J'ai teste le remplacement de contenu en utilisant preg_match et preg_replace.

--- test/test.php
+++ test/test.php
@@ -1,27 +1,13 @@
 <?php
-$contenu1 = "[% block titre %]<title>PPE 2</title>[% finblock %]";
-$contenu2 = "[% block titre %]<title>PPE Test</title>[% finblock %]";
-$pattern1 = '/\[\% block \"(.*?)\" \%\]/';
-$pattern3 = '/\[\% finblock \%\]/';
-$pattern4 = '/\[\% block \"(.*?)\" \%\](.*)\[\% finblock \%\]/';
+$contenu = '[% block titre %]test[% finblock %]';
+$contenu2 = '[% block titre %]<title>kjvhkirvbr</title>[% finblock %]';
 
-if(preg_match($pattern1, $contenu, $sortie)){
-	if(preg_match($pattern2, $contenu, $sortie2)){
-		preg_match(pattern, subject);
-		preg_replace(pattern, replacement, subject);
-	}
-}
+$pattern = '/\[\% block titre %\](.*)\[\% finblock \%\]/';
 
-foreach ($variable as $key => $value) {
-	# code...
-}
+preg_match($pattern, $contenu, $sortie);
+preg_match($pattern, $contenu2, $sortie2);
 
-print_r($sortie);
-echo $sortie[1];
+$test = preg_replace($pattern, $sortie2[1], $contenu);
 
-if(file_exists("../gabarit/vue/".$sortie[1])){
-	echo "Oui!";
-}else{
-	echo "Non!";
-}
+print_r($test);
 ?>
