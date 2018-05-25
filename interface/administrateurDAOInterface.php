2baff2719cb50a84ed63ad36186d5ada92c3e9bf
Client <client@client.com>
2018-05-25 09:58:32 +0200

    J'ai suprimmé le répertoire 'interface' car il n'avait aucune utilité.

--- interface/administrateurDAOInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-interface administrateurDAOInterface{
-	public function connecter($email, $mot_de_passe);
-	public function inscrire($administrateur_super, $administrateur_nom, $administrateur_prenom, $administrateur_mot_de_passe_hash,
-							 $administrateur_telephone, $administrateur_email, $administrateur_adresse,
-							 $administrateur_ville, $administrateur_code_postal);
-}
-?>
